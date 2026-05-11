8d55a64100e333dd5d2df7ac4077ec42e916be27
Charly Martin <charly@hey.com>
2026-05-11 16:41:05 +0100

    feat: restructure collections module and add collection creation functionality

--- packages/qino/src/index.ts
+++ packages/qino/src/index.ts
@@ -1,3 +1,3 @@
 export { createConfig, type Config } from "./runtime/create-config";
-export { createCollection } from "./runtime/create-collections";
+export { createCollection } from "./runtime/collections";
 export type { Collection } from "./types";
