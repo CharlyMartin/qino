54d91bf26a29456bf93ab0de70fcb116e0f6a5cf
Charly Martin <charly@hey.com>
2026-05-11 16:44:28 +0100

    feat: implement registry module for managing collections

--- packages/qino/src/runtime/registry/index.ts
+++ packages/qino/src/runtime/registry/index.ts
@@ -1,5 +1,5 @@
-import type { AnyCollection } from "../types";
-import { QinoMeta } from "./globals";
+import type { AnyCollection } from "../../types";
+import { QinoMeta } from "../globals";
 
 const REGISTRY_KEY = Symbol.for("qino.registry");
 
