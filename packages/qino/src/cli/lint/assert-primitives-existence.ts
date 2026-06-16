d152539e52ce70bdd8de22028082df790f6b8275
Charly Martin <charly@hey.com>
2026-06-16 17:30:51 +0200

    refactor: update assertPrimitivesExistence to print a warning instead of throwing an error

--- packages/qino/src/cli/lint/assert-primitives-existence.ts
+++ packages/qino/src/cli/lint/assert-primitives-existence.ts
@@ -2,10 +2,6 @@ import { ROOT_FOLDER_NAME } from "../../data";
 import type { AnyPrimitive } from "../../types/utils";
 
 export function assertPrimitivesExistence(primitives: Array<AnyPrimitive>) {
-  // instead of erroring out, maybe here we should just print a warning that no primitives were found.
-  // Tell the user to create a collection, singleton, or tree under the "qino/" folder.
-  // Exit the process with code 0 instead of throwing an error.
-
   if (primitives.length == 0) {
     throw new Error(
       `No primitives found under "${ROOT_FOLDER_NAME}/". Define at least one collection, singleton, or tree.`,
