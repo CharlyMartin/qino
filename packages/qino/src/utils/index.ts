a76dbc0387fc3bacdc9e15117e5dce25c006d072
Charly Martin <charly@hey.com>
2026-09-12 16:21:10 +0100

    fixup! feat: remove json and markdown file name schemas and their tests; update post schema to use string types for categories and author

--- packages/qino/src/utils/index.ts
+++ packages/qino/src/utils/index.ts
@@ -2,11 +2,5 @@ export { assertDirectory } from "./assert-directory";
 export { assertFile } from "./assert-file";
 export { isDirectory } from "./is-directory";
 export { isFile } from "./is-file";
-export { mdxFileNameSchema } from "./mdx-file-name-schema";
-
-import { stats } from "./markdown";
-
+export { type MarkdownStats, markdown } from "./markdown";
 export { removeLeadingSlash } from "./remove-leading-slash";
-
-export const markdown = { stats } as const;
-export type { MarkdownStats } from "./markdown";
